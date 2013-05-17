0e289ec8ba510e0425a3167cbe57988f819fbe76
Andrew Livesay <andrew.livesay@gmail.com>
2013-05-16 17:28:13 -0700

    playing with irc rpg concepts

--- omegadoom/plugins/rpg.py
+++ omegadoom/plugins/rpg.py
@@ -8,12 +8,14 @@ from omegadoom.util import OmegaDoomUtil
 
 
 def roll(notation):
-  # http://stackoverflow.com/questions/1031466/evaluate-dice-rolling-notation-strings
-  f=lambda s:sum(int(c or 0)+sum(random.randint(1,int(b))for i in[0]*int(a or 1))for a,b,c in re.findall(r'(\d*)d(\d+)(\s*[+-]\s*\d+)?',s))
-  return f(notation)
+    # http://stackoverflow.com/questions/1031466/evaluate-dice-rolling-notation-strings
+    f=lambda s:sum(int(c or 0)+sum(random.randint(1,int(b))for i in[0]*int(a or 1))for a,b,c in re.findall(r'(\d*)d(\d+)(\s*[+-]\s*\d+)?',s))
+    return f(notation)
 
-
+OmegaDoomRPGPlayer = namedtuple('RPGPlayer', ['credits'])
 OmegaDoomRPGWeapon = namedtuple('RPGWeapons', ['damage', 'cost'])
+
+players = {}
 
 weapons = { 'pair of fists' : OmegaDoomRPGWeapon(1, 0),
             'stick' : OmegaDoomRPGWeapon(2, 50),
@@ -21,36 +23,28 @@ weapons = { 'pair of fists' : OmegaDoomRPGWeapon(1, 0),
 
 class OmegaDoomPlugin(OmegaDoomPluginBase):
 
-  commands = ['roll', 'exp', 'inventory', 'search']
-  
-  def __init__(self, config):
-    OmegaDoomPluginBase.__init__(self, config)
-    self._redis = redis.Redis(config['redis_host'], config['redis_port'])
+    commands = ['roll', 'exp', 'inventory', 'search']
 
-  def run_command(self, protocol, command, data, privmsg):
-    prefix, channel, message = privmsg
-    nick, ident, host = OmegaDoomUtil.parse_prefix(prefix)
+    def run_command(self, protocol, command, data, privmsg):
+        prefix, channel, message = privmsg
+        nick, ident, host = OmegaDoomUtil.parse_prefix(prefix)
 
-    target = nick if channel == self.config['nickname'] else channel
+        target = nick if channel == self.config['nickname'] else channel
 
-    if command == 'inventory':
-      protocol.msg(target, 'Not implemented yet.')
+        if command == 'inventory':
+            protocol.msg(target, 'Not implemented yet.')
 
-    if command == 'roll':
-      if data:
-        protocol.msg(target, str(roll(data)))
+        if command == 'roll':
+            if data:
+                protocol.msg(target, str(roll(data)))
 
-    if command == 'search':
-      search_check = roll('1d10')
-      if search_check <= 2:
-        w = random.choice(list(weapons.keys()))
-        protocol.msg(target, 'You found a %s!' % (w))
-      elif search_check <= 5:
-        credits = roll('1d100')
-        protocol.msg(target, 'You found %s credits!' % (credits))
-      else:
-        protocol.msg(target, 'You found... nothing.')
-
-
-
-    
+        if command == 'search':
+            search_check = roll('1d10')
+            if search_check <= 2:
+                w = random.choice(list(weapons.keys()))
+                protocol.msg(target, 'You found a %s!' % (w))
+            elif search_check <= 5:
+                player_credits = roll('1d100')
+            protocol.msg(target, 'You found %s credits!' % (player_credits))
+        else:
+            protocol.msg(target, 'You found... nothing.')
